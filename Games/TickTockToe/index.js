ae05d69ff7a0d3decca14c21ec45b35ba5ebb73a
Ashish-Aundhe <123938575+Ashish-Aundhe@users.noreply.github.com>
2023-03-27 00:44:34 +0530

    Add files via upload

---
+++ Games/TickTockToe/index.js
@@ -0,0 +1,64 @@
+console.log("Welcome")
+let turn = "1"
+let isgameover = false;
+
+//function to change the turn 
+
+const changeTurn =  ()=>{
+    return turn === "1"?"0":"1"
+}
+
+//function to check for a win 
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins =[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 5],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ] 
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + "Won"
+            isgameover = true
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+        }
+    })
+    
+}
+
+//Game Logic 
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+  let boxtext = element.querySelector('.boxtext');
+  element.addEventListener('click',(e)=>{
+    if(boxtext.innerText === ''){
+        boxtext.innerText = turn;
+        turn = changeTurn();
+        // changeTurn();
+        checkWin();
+        if(!isgameover){
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+
+        }
+       
+
+    }
+  })
+})
+//All onclick listener to reset button
+resizeTo.addEventListener('.click', ()=>{
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element=>{
+        element.innerText = ""
+    });
+    turn ="1";
+    isgameover= false
+    document.getElementsByClassName("info")[0].innerText = "turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+})
+  
